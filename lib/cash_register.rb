d60dc843d725e6c08586e04332c88de5231dc36d
Anita Beauchamp <amadamspr@gmail.com>
2018-07-28 23:34:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    @items << title unless self.items.include?(title)
+    @items << title unless @items.include?(title)
   end
 
   def apply_discount
